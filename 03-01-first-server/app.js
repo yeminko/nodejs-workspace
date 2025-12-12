e581ed80e6350853b488cfae49244766f1119ad0
yeminko <yeminko47@gmail.com>
2025-12-12 09:15:42 +0630

    Implement form submission handling and save messages to a file

--- 03-01-first-server/app.js
+++ 03-01-first-server/app.js
@@ -15,10 +15,21 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "Dummy message");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
 
   res.setHeader("Content-Type", "text/html");
