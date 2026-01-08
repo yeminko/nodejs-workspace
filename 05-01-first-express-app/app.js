a51f752f36fd85b968f4283a6f766b408d30a1b0
yeminko <yeminko47@gmail.com>
2026-01-08 09:14:07 +0630

    feat: implement admin and shop routes with product form handling

--- 05-01-first-express-app/app.js
+++ 05-01-first-express-app/app.js
@@ -1,15 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Add Product</h1>");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express.js!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
