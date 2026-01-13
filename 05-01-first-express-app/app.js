02c5c56912dfbf04d654b325162c005dafdab085
yeminko <yeminko47@gmail.com>
2026-01-13 11:53:12 +0630

    feat: add product management views and 404 error handling

--- 05-01-first-express-app/app.js
+++ 05-01-first-express-app/app.js
@@ -3,12 +3,17 @@ const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3000);
