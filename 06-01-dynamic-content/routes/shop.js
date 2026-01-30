070c1c3fa781e2f60585f4dc02c5a50bf05f4f0a
yeminko <yeminko47@gmail.com>
2026-01-30 18:33:35 +0630

    feat: switch view engine to Handlebars, update 404 page, and refactor product views

--- 06-01-dynamic-content/routes/shop.js
+++ 06-01-dynamic-content/routes/shop.js
@@ -11,7 +11,8 @@ router.get("/", (req, res, next) => {
   const products = adminData.products;
   res.render("shop", {
     prods: products,
-    docTitle: "My Shop",
+    pageTitle: "My Shop",
+    path: "/",
   });
 });
 
