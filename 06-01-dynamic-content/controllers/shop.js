3f55681ee744477c57fbaf3c6acf9f2aacca42d8
yeminko <yeminko47@gmail.com>
2026-02-25 08:42:52 +0630

    feat: enhance product management with additional fields and orders view

--- 06-01-dynamic-content/controllers/shop.js
+++ 06-01-dynamic-content/controllers/shop.js
@@ -24,6 +24,10 @@ exports.getCart = (req, res, next) => {
   res.render("shop/cart", { path: "/cart", pageTitle: "Your Cart" });
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", { path: "/orders", pageTitle: "Your Orders" });
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", { path: "/checkout", pageTitle: "Checkout" });
 };
