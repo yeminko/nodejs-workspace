664dd5d314b04c6fbbc8720a3b8c6dfbb18849fd
yeminko <yeminko47@gmail.com>
2026-04-06 10:08:11 +0630

    refactor: standardize code formatting and improve database connection handling

--- 12-01-mongodb/util/database.js
+++ 12-01-mongodb/util/database.js
@@ -2,8 +2,27 @@ require("dotenv").config();
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect(process.env.MONGO_DB_URL)
-  .then()
-  .catch((err) => {
-    console.log(err);
-  });
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(process.env.MONGO_DB_URL)
+    .then((client) => {
+      console.log("Connected to database");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
