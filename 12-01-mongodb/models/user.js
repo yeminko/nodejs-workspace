4f8d46bf4b49bc13633a8e9d66d740808d66f0a1
yeminko <yeminko47@gmail.com>
2026-04-23 08:37:27 +0630

    feat: refactor cart functionality and update product rendering in views

--- 12-01-mongodb/models/user.js
+++ 12-01-mongodb/models/user.js
@@ -18,7 +18,7 @@ class User {
 
   addToCart(product) {
     const cartProductIndex = this.cart.items.findIndex((cp) => {
-      return cp.productId === product._id;
+      return cp.productId.toString() === product._id.toString();
     });
 
     let newQuantity = 1;
@@ -45,6 +45,56 @@ class User {
         { _id: new ObjectId(this._id) },
         { $set: { cart: updatedCart } },
       );
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } },
+      );
+  }
+
+  addOrder() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .insertOne(this.cart)
+      .then((result) => {
+        this.cart = { items: [] };
+        return db
+          .collection("users")
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } },
+          );
+      });
   }
 
   static findById(userId) {
