1a64a3b72f450a2c0d85cb4441a200f44146cb6a
yeminko <yeminko47@gmail.com>
2026-05-07 08:31:50 +0630

    feat: enhance user and product models with associations, implement cart functionality and update views

--- 13-01-working-with-mongoose/app.js
+++ 13-01-working-with-mongoose/app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -20,14 +20,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("69d85a1e23d9f8881bf2df77")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("69fbed0846458f3c76a7acee")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,17 @@ app.use(errorController.get404);
 mongoose
   .connect(process.env.MONGO_DB_URL)
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "John Doe",
+          email: "john@example.com",
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    });
+
     app.listen(3000);
   })
   .catch((err) => {
