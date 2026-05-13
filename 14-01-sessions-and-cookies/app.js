acfe71a1521e02fefdfe8de8f9a1543fb521df25
yeminko <yeminko47@gmail.com>
2026-05-13 08:50:10 +0630

    feat: initialize project with sessions and cookies setup
    - Add package.json with dependencies and scripts
    - Create CSS files for authentication, cart, forms, main styles, orders, and products
    - Implement JavaScript for mobile navigation and backdrop functionality
    - Set up Express routes for admin, authentication, and shop functionalities
    - Create EJS views for 404 error, admin product management, authentication, and shop pages
    - Include reusable EJS components for head, navigation, and end scripts

---
+++ 14-01-sessions-and-cookies/app.js
@@ -0,0 +1,57 @@
+const path = require("path");
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("69fbed0846458f3c76a7acee")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Max",
+          email: "max@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
