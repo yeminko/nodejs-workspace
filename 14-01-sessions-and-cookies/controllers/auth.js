6e63dff10c5174805094a50a7bcca38692f50a82
yeminko <yeminko47@gmail.com>
2026-05-15 15:30:17 +0630

    feat: add authentication checks to views and controllers for improved user experience

--- 14-01-sessions-and-cookies/controllers/auth.js
+++ 14-01-sessions-and-cookies/controllers/auth.js
@@ -1,11 +1,13 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").trim().split("=")[1] === "true";
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
   res.redirect("/");
 };
