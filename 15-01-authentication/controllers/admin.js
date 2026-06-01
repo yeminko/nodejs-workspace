0ba7dd95f8d63cd289fccd90515b4a5b93ea9187
yeminko <yeminko47@gmail.com>
2026-06-01 08:43:55 +0630

    feat: add CSRF protection and update views to include CSRF tokens

--- 15-01-authentication/controllers/admin.js
+++ 15-01-authentication/controllers/admin.js
@@ -9,7 +9,6 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
-    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
@@ -53,7 +52,6 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
-        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
@@ -91,7 +89,6 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
-        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
