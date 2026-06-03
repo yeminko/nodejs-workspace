32ec801a0dd465a945fd9d0618e68dad735358f1
yeminko <yeminko47@gmail.com>
2026-06-03 14:51:29 +0630

    Add user message display for error notifications and update styles
    - Added "connect-flash" for flash message handling in package.json.
    - Introduced user message styles in main.css for error notifications.
    - Implemented error message display in login.ejs and signup.ejs views.

--- 15-01-authentication/controllers/auth.js
+++ 15-01-authentication/controllers/auth.js
@@ -1,17 +1,45 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.SEND_GRID_API_KEY,
+    },
+  }),
+);
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
+    errorMessage: message,
   });
 };
 
@@ -22,6 +50,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
+        req.flash("error", "Invalid email or password.");
         return res.redirect("/login");
       }
 
@@ -37,6 +66,7 @@ exports.postLogin = (req, res, next) => {
             });
           }
 
+          req.flash("error", "Invalid email or password.");
           return res.redirect("/login");
         })
         .catch((err) => {
@@ -55,6 +85,10 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash(
+          "error",
+          "E-Mail exists already, please pick a different one.",
+        );
         return res.redirect("/signup");
       }
       return bcrypt
