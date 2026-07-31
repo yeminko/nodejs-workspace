f6a4964ea78064b2e0ca3d4bef10f7551bce9ff5
yeminko <yeminko47@gmail.com>
2026-07-31 14:22:33 +0630

    feat: implement get and create post functionality in feed controller and update routes

--- 24-01-rest-api/controllers/feed.js
+++ 24-01-rest-api/controllers/feed.js
@@ -1,3 +1,37 @@
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({ message: "Posts fetched successfully" });
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/first-post.jpg",
+        creator: {
+          name: "Maximilian",
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  const post = {
+    _id: new Date().toISOString(),
+    title: title,
+    content: content,
+    imageUrl: "images/first-post.jpg",
+    creator: {
+      name: "Maximilian",
+    },
+    createdAt: new Date(),
+  };
+
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: post,
+  });
 };
