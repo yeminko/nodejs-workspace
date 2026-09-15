d93d754814d8a55b6aed86d637da760b26f04d20
yeminko <yeminko47@gmail.com>
2026-09-15 15:57:00 +0630

    refactor: standardize string quotes and improve error handling in auth controller and middleware

---
+++ 30-01-testing-nodejs/test/auth-controller.js
@@ -0,0 +1,28 @@
+const expect = require("chai").expect;
+const sinon = require("sinon");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+const AuthController = require("../controllers/auth");
+
+describe("Auth Controller - Login", function () {
+  it("should throw an error with code 500 if accessing the database fails", function (done) {
+    sinon.stub(User, "findOne");
+    User.findOne.throws();
+
+    const req = {
+      body: {
+        email: "test@test.com",
+        password: "testpassword",
+      },
+    };
+
+    AuthController.login(req, {}, () => {}).then((result) => {
+      expect(result).to.be.an("error");
+      expect(result).to.have.property("statusCode", 500);
+      done();
+    });
+
+    User.findOne.restore();
+  });
+});
--- 30-01-testing-nodejs/test/auth-middleware.js
+++ 30-01-testing-nodejs/test/auth-middleware.js
@@ -1,4 +1,6 @@
 const expect = require("chai").expect;
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
 
 const authMiddleware = require("../middleware/is-auth");
 
@@ -23,5 +25,31 @@ describe("Auth Middleware", function () {
     };
 
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should throw an error if the token cannot be verified", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer invalidtoken";
+      },
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yield a userId if the token is valid", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer validtoken";
+      },
+    };
+
+    sinon.stub(jwt, "verify").returns({ userId: "abc" });
+
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+    expect(req.userId).to.equal("abc");
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
   });
 });
